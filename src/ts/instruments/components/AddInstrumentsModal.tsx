7a43370d4ef1e56f415ce92ec814a6868a2653c2
Nicholas Herrick <nicholas.herrick95@gmail.com>
2025-10-02 17:39:05 -0600

    Complete logic to store instruments in local storage

--- src/ts/instruments/components/AddInstrumentsModal.tsx
+++ src/ts/instruments/components/AddInstrumentsModal.tsx
@@ -1,17 +1,23 @@
 import Modal from "react-bootstrap/Modal";
-import { type AddModalProps } from "../types";
+import { type AddModalProps, type AddInstrumentFormValues } from "../types";
 import AddInstrumentsFields from "./AddInstrumentsFields";
 import { Form } from "react-final-form";
 import Button from "../../shared/components/Button";
+import useLocalStorage from "../../shared/hooks/useLocalStorage";
+import { LocalStorageKeys } from "../../shared/constants";
 
 export const AddInstrumentsModal = ({ show, handleHide }: AddModalProps) => {
-  const onSubmit = () => {};
+  const { setLocalStorage } = useLocalStorage();
+
+  const onSubmit = (values: AddInstrumentFormValues) => {
+    setLocalStorage(LocalStorageKeys.InstrumentList, values);
+  };
 
   return (
     <Modal show={show} onHide={handleHide} size="lg">
       <Form onSubmit={onSubmit}>
-        {({ pristine, submitting }) => (
-          <form>
+        {({ handleSubmit, pristine, submitting }) => (
+          <form onSubmit={handleSubmit}>
             <Modal.Header closeButton>
               <Modal.Title>Add New Instrument</Modal.Title>
             </Modal.Header>
--- src/ts/instruments/types.ts
+++ src/ts/instruments/types.ts
@@ -2,3 +2,9 @@ export interface AddModalProps {
   show: boolean;
   handleHide: () => void;
 }
+
+export interface AddInstrumentFormValues {
+  name: string;
+  ImageURL: string;
+  SterilizationType: string;
+}
